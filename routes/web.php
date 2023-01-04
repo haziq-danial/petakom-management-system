26af6a4e3deec03ea15291df54382ee9a35ad9e1
HazmiHazim <hazimnetto01@gmail.com>
2023-01-04 21:08:55 +0800

    added login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(LoginController::class)->group(function(){
+
+    Route::get('login', 'index')->name('login');
+
+    Route::get('registration', 'registration')->name('registration');
+
+    Route::get('logout', 'logout')->name('logout');
+
+});
+
+Route::controller(RegistrationController::class)->group(function(){
+
+    Route::post('validate_registration', 'validate_registration')->name('user.validate_registration');
+
+    //Route::post('validate_login', 'validate_login')->name('user.validate_login');
+
+    //Route::get('dashboard', 'dashboard')->name('dashboard');
+
+});
