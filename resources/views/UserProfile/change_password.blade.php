a1ddea6040fbd87d68979bac2777d5ce0b64440a
Haziq Danial <haziqdanial031@gmail.com>
2023-01-27 14:18:52 +0800

    updated manage profile ui

--- resources/views/UserProfile/change_password.blade.php
+++ resources/views/UserProfile/change_password.blade.php
@@ -1,4 +1,11 @@
-@extends('dashboard')
+@extends('layouts.app')
+
+@section('stylesheet')
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css ') }}">
+@endsection
 @section('content')
 <div class="row mt-4">
 
@@ -8,7 +15,7 @@
     </div>
     @endif
 
-    <div class="col-md-11">  
+    <div class="col-md-11">
         <div class="card">
             <div class="card-header">Change Password</div>
             <div class="card-body">
@@ -38,4 +45,15 @@
 </div>
 
 
+@endsection
+
+@section('scripts')
+    <!-- jQuery -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- AdminLTE App -->
+    <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 @endsection
